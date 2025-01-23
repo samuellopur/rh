21dadd8fe31479c7a9696783f97ac55cc5479d0e
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-22 20:21:06 -0500

    Se crea ControladorREST

---
+++ src/main/java/sl/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,21 @@
+package sl.rh.controlador;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sl.rh.servicio.EmpleadoServicio;
+
+@RestController
+@RequestMapping("rh-app") //http://localhost:8080/rh-app/
+@CrossOrigin(value = "http://localhost:3000")
+public class EmpleadoControlador {
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    private final EmpleadoServicio empleadoServicio;
+
+    public EmpleadoControlador(EmpleadoServicio empleadoServicio) {
+        this.empleadoServicio = empleadoServicio;
+    }
+}
