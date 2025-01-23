102dcbd4d88df5cc0d1165e171cc629204c2da7a
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-22 21:36:07 -0500

    Peticion Get, Listado de Empleados desde ControladorREST

--- src/main/java/sl/rh/controlador/EmpleadoControlador.java
+++ src/main/java/sl/rh/controlador/EmpleadoControlador.java
@@ -3,9 +3,14 @@ package sl.rh.controlador;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sl.rh.modelo.Empleado;
 import sl.rh.servicio.EmpleadoServicio;
+import sl.rh.servicio.IEmpleadoServicio;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rh-app") //http://localhost:8080/rh-app/
@@ -13,9 +18,18 @@ import sl.rh.servicio.EmpleadoServicio;
 public class EmpleadoControlador {
     private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
 
-    private final EmpleadoServicio empleadoServicio;
+    private final IEmpleadoServicio empleadoServicio;
 
     public EmpleadoControlador(EmpleadoServicio empleadoServicio) {
         this.empleadoServicio = empleadoServicio;
     }
+
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados(){
+        List<Empleado> empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach(empleado -> logger.info(empleado.toString()));
+        return empleados;
+    }
+
+
 }
--- src/main/java/sl/rh/modelo/Empleado.java
+++ src/main/java/sl/rh/modelo/Empleado.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
 @Data
@@ -21,4 +18,5 @@ public class Empleado {
     private String nombre;
     private String departamento;
     private Double sueldo;
+
 }
