2a6f79ce5bad4f140d10afd9c7039ca2b12db49b
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-30 20:38:11 -0500

    buscarEmpleadoPorId (EmpleadoControlador) se crea paquete y clase de excepcion

--- src/main/java/sl/rh/controlador/EmpleadoControlador.java
+++ src/main/java/sl/rh/controlador/EmpleadoControlador.java
@@ -2,7 +2,9 @@ package sl.rh.controlador;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sl.rh.excepcion.RecursoNoEncontradoExcepcion;
 import sl.rh.modelo.Empleado;
 import sl.rh.servicio.EmpleadoServicio;
 import sl.rh.servicio.IEmpleadoServicio;
@@ -32,5 +34,14 @@ public class EmpleadoControlador {
     public Empleado agregarEmpleado(@RequestBody Empleado empleado){
         logger.info("Empleado a agregar: " + empleado);
         return empleadoServicio.guardarEmpleado(empleado);
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> obtenerEmpleadoPorId(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null)
+            throw new RecursoNoEncontradoExcepcion("Id no encontrado: " + id);
+        return ResponseEntity.ok(empleado);
+
     }
 }
